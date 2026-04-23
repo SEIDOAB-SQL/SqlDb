36c2ee0bb508db2416ef65c69c71309802c5a176
Martin <martin_lenart@icloud.com>
2026-04-23 09:50:17 +0200

    update

--- lesson02/01-strings-numeric/scripts/01-collation.sql
+++ lesson02/01-strings-numeric/scripts/01-collation.sql
@@ -22,5 +22,27 @@ inner join sys.tables t on c.object_id = t.object_id;
 -- Set the collation in a query to override the default collation of the database
 SELECT PetId, Name, AnimalKind
 FROM dbo.Pet
-WHERE Name = 'whiskers' COLLATE Latin1_General_CI_AS;
+WHERE Name = 'whisker' COLLATE Latin1_General_CS_AS;
 GO
+
+-- Set the collation of the Pet table Name column to Case Sensitive (CS)
+-- Latin1_General_CS_AS: Case Sensitive, Accent Sensitive
+ALTER TABLE [dbo].[Pet]
+    ALTER COLUMN [Name] [nvarchar](50) COLLATE Latin1_General_CS_AS NULL;
+GO
+
+
+-- Set the collation of the friends database to Case Sensitive (CS)
+-- Latin1_General_CS_AS: Case Sensitive, Accent Sensitive
+-- Note: existing column collations are NOT changed automatically;
+--       columns that inherited the old collation must be altered separately.
+USE tempdb;
+GO
+
+ALTER DATABASE friends
+    COLLATE Latin1_General_CS_AS;
+GO
+
+USE friends;
+GO
+
