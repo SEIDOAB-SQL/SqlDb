68110d28f740bbed6939a720339a3465c5712f37
Martin <martin_lenart@icloud.com>
2026-05-18 12:27:30 +0200

    update

--- lesson14/04-ownership-chaining/scripts/05-list-sp-udf.sql
+++ lesson14/04-ownership-chaining/scripts/05-list-sp-udf.sql
@@ -2,13 +2,15 @@ USE [friends];
 GO
 
 
--- List all stored procedures, scalar UDFs, and table-valued functions
-SELECT SCHEMA_NAME(schema_id) + '.' + name AS ObjectName,
-       type_desc                            AS ObjectType
+-- List all stored procedures, scalar UDFs, table-valued functions, and views
+SELECT SCHEMA_NAME(schema_id)                AS SchemaName,
+       name                                  AS ObjectName,
+       type_desc                             AS ObjectType
 FROM   sys.objects
 WHERE  type_desc IN (
            'SQL_STORED_PROCEDURE',
            'SQL_SCALAR_FUNCTION',
            'SQL_INLINE_TABLE_VALUED_FUNCTION',
-           'SQL_TABLE_VALUED_FUNCTION')
-ORDER BY type_desc, ObjectName;
+           'SQL_TABLE_VALUED_FUNCTION',
+           'VIEW')
+ORDER BY SchemaName, type_desc, ObjectName;
