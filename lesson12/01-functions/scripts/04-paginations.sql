58e9edd91ea66fac2cafe6aaa1b08ea5fdce6461
Martin <martin_lenart@icloud.com>
2026-05-18 10:54:45 +0200

    update

---
+++ lesson12/01-functions/scripts/04-paginations.sql
@@ -0,0 +1,36 @@
+USE sakila;
+GO
+
+DROP FUNCTION IF EXISTS dbo.fn_GetFilmPage;
+GO
+
+CREATE FUNCTION dbo.fn_GetFilmPage
+(
+    @PageNumber INT,   -- 1-based page number
+    @PageSize   INT    -- number of rows per page
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT  film_id,
+            title,
+            description,
+            release_year,
+            rating,
+            length,
+            rental_duration,
+            rental_rate,
+            replacement_cost,
+            special_features
+    FROM    dbo.film
+    ORDER BY film_id
+    OFFSET  (@PageNumber - 1) * @PageSize ROWS
+    FETCH NEXT @PageSize ROWS ONLY
+);
+GO
+
+-- Usage examples
+SELECT * FROM dbo.fn_GetFilmPage(1, 10);   -- first page,  10 rows
+SELECT * FROM dbo.fn_GetFilmPage(2, 10);   -- second page, 10 rows
+SELECT * FROM dbo.fn_GetFilmPage(5, 20);   -- fifth page,  20 rows
