0fd0115200f20a8a5276bd8d41516520cf796b7e
Martin <martin_lenart@icloud.com>
2026-05-18 10:15:21 +0200

    update

--- lesson10/02-case/scripts/01-case.sql
+++ lesson10/02-case/scripts/01-case.sql
@@ -1,7 +1,20 @@
 USE sakila;
 GO
 
---simple case expression
+--Simple case expression
+SELECT first_name, last_name,
+    --simple case stement produces a column in the SELCT statement 
+    CASE active
+        WHEN 1 THEN 'Is an active customer'
+
+        --do you remember the CONVERT statement from datetime -> string conversion
+        WHEN 0 THEN 'In not an active customer, since ' + CONVERT(NVARCHAR, last_update, 111)
+        ELSE 'Unknown status'
+    END AS [customer status]
+FROM dbo.customer
+WHERE customer_id IN (1,5,16,357,358,592,7);
+
+--searched case expression
 SELECT first_name, last_name, store_id,
     --simple case stement produces a column in the SELCT statement 
     CASE 
